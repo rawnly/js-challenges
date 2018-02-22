9b615291daa5b47c88ba010c8316b332abdc3c98
Federico Vitale <fedevitale99@gmail.com>
2018-02-22 22:17:16 +0100

    removed old tests

--- tests/_array-merge.js
+++
@@ -1,8 +0,0 @@
-const test = require('ava');
-const {arrayMerge} = require('..');
-
-test('ARRAY: MERGE', t => {
-	var output = arrayMerge([1, 2], [3, 4]);
-
-	return t.deepEqual(output, [1, 2, 3, 4]);
-})
--- tests/_array-sum.js
+++
@@ -1,8 +0,0 @@
-const test = require('ava');
-const {arraySum} = require('..');
-
-test('ARRAY: SUM', t => {
-	var output = arraySum([1, 2, 3]);
-
-	return t.deepEqual(output, 6);
-})
