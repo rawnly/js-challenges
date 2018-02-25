a6dea1e150178f9ac0181f77f337ed35e55deb2d
Federico Vitale <fedevitale99@gmail.com>
2018-02-25 16:06:24 +0100

    Added JSDOC instead of comments

--- src/challenges.js
+++ src/challenges.js
@@ -1,77 +1,279 @@
-// CHALLENGE 1: ROUND NUMBER
-// DESCRIPTION: Write a function that round a number to given decimal places.
-// Ex: round(Math.PI, 2) => 3.14 (By default it will return 0 decimal places, round(Math.PI, 2) => 3)
+/**
+ * CHALLENGE 1: ROUND NUMBER
+ * @name round
+ * @description Write a function that round a number (`n`) to given decimal places.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ *
+ * 
+ * @example Usage:
+ * round(Math.PI, 2) // => 3.14
+ * 
+ * 
+ * @param {Number} n The number to be rounded
+ * @param {Number} places Decimal places
+ * 
+ * @returns {Number} Rounded number
+ */
 module.exports.round = function(n, places = 0) {}
 
 
-// CHALLENGE 2: MERGE MULTIPLE ARRAYS
-// DESCRIPTION: Write a function that merges multiple given arrays.
-// EX: ArrayMerge([1, 2], ['a', 'b']) => [1, 2, 'a', 'b']
+/**
+ * CHALLENGE 2: MERGE MULTIPLE ARRAYS
+ * @name arrayMerge
+ * @description Write a function that merges multiple given arrays.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * ArrayMerge([1, 2], ['a', 'b']) //=>  [1, 2, 'a', 'b']
+ * 
+ * 
+ * @param {Array} arr Arrays to merge
+ * 
+ * @returns {Array} Returns the merge of all arrays
+ */
 module.exports.arrayMerge = function(...arr) {}
 
 
-// CHALLENGE 3: SUM CONTENT OF AN ARRAY
-// DESCRIPTION: Write a function to sum the content of an array.
-// EX: sumArray([1, 2]) => 3
+/**
+ * CHALLENGE 3: SUM CONTENT OF AN ARRAY
+ * 
+ * @name arraySum
+ * @description Write a function to sum the content of an array
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * arraySum([10, 10, 10]) //=> 30
+ * 
+ * 
+ * @param {Array} arr Array to sum
+ * 
+ * @return {Number} The sum of all items in the array
+ */
 module.exports.arraySum = function(arr) {}
 
 
-// CHALLENGE 4: OBJECT FOREACH
-// DESCRIPTION: Write a forEach function that works with Objects.
-// EX: objectForEach({ a: 1, b: 2, c: 3 }, console.log(key, value)) => a, 1; b, 2; etc...
+/**
+ * CHALLENGE 4: OBJECT FOREACH
+ * @name objectForEach
+ * @description ForEach function that works with Objects.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * objectForEach({ a:1, b:2 }, (key, value) => console.log(key, value)) //=> { a:1, b:2 }
+ * 
+ * 
+ * @param {Object} obj Main Object
+ * @param {Function} callback Function callback
+ * 
+ * @returns {Object} The start object
+ */
 module.exports.objectForEach = function(obj, callback) {}
 
 
-// CHALLENGE 5: STRING REVERSE
-// DESCRIPTION: Write a function that reverse a string.
-// Ex: reverseString('hello') => 'ollah'
+/**
+ * CHALLENGE 5: REVERSE STRING
+ * @name reverseString
+ * @description A function that reverse a string.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * reverseString('hello') //=> 'ollah'
+ * 
+ * 
+ * @param {String} str String to be reversed.
+ * 
+ * @returns {String} String reversed.
+ */
 module.exports.reverseString = function(str) {}
 
 
-// CHALLENGE 6: CHECK PALINDROME
-// DESCRIPITION: Write a function that checks if a word is a palindrome.
-// Ex: isPalindrome('racecar') => true
+/**
+ * CHALLENGE 6: CHECK PALINDROME
+ * @name isPalindrome
+ * @description A function that checks if a word is a palindrome.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * isPalindrome('racecar') //=> true
+ * 
+ * 
+ * @param {String} str String to be checked for palindrome.
+ * 
+ * @returns {Boolean} Return true if is a palindrome.
+ */
 module.exports.isPalindrome = function(str) {}
 
 
-// CHALLENGE 7: IS MULTIPLE OF
-// DECSRIPTION: Write a function that checks if a number is multiple of another number.
-// Ex: isMultipleOf(3, 15) => true
+/**
+ * CHALLENGE 7: IS MULTIPLE OF
+ * @name isMultipleOf
+ * @description A function that checks if a number is multiple of another number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * isMultipleOf(3, 15) //=> true
+ * 
+ * 
+ * @param {Number} a Number to be checked for multiple of `b`
+ * @param {Number} b 
+ * 
+ * @returns {Boolean} If `a` is multiple of `b` returns true
+ */
 module.exports.isMultipleOf = function(a, b) {}
 
 
-// CHALLENGE 8: GET THE LONGEST WORD
-// DECSRIPTION: Write a function that returns the longest word of a sentence.
-// Ex: longestWord('short loooong l0000ng') => 'loooong'
+/**
+ * CHALLENGE 8: GET THE LONGEST WORD
+ * @name longestWord
+ * @description A function that returns the longest word of a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * longestWord('short loooong l0000ng') //=> 'loooong'
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {String} Returns the longest word of `str`
+ */
 module.exports.longestWord = function(str) {}
 
 
-// CHALLENGE 9: CAPITALIZE
-// DECSRIPTION: Write a function that capitalize each word in a sentence.
-// Ex: capitalize('hello world') => 'Hello World'
+/**
+ * CHALLENGE 9: CAPITALIZE
+ * @name capitalize
+ * @description A function that capitalize each word in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * capitalize('hello world') //=> 'Hello World'
+ * 
+ * 
+ * @param {String} str The string to be capitalized 
+ * 
+ * @returns {String} Returns a capitalized string
+ */
 module.exports.capitalize = function(str) {}
 
 
-// CHALLENGE 10: VOWEL COUNT
-// DECSRIPTION: Write a function that count vowel in a sentence.
-// Ex: vowelCount('fox') => 1
-module.exports.vowelCount = function (str) {}
+
+/**
+ * CHALLENGE 10: VOWEL COUNT
+ * @name vowelCount
+ * @description A function that count vowel in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * vowelCount('fox') //=> 1
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {Number} Returns number of vowels in `str`
+ */
+module.exports.vowelCount = function(str) {}
 
 
-// CHALLENGE 11: MAX CHAR
-// DESCRIPTION: Get the most used char in a sentence.
-// Ex: maxChar('hello') => l
+
+/**
+ * CHALLENGE 11: MAX CHAR
+ * @name maxChar
+ * @description Get the most used char in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * maxChar('hello') //=> l
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {Number} Returns character most used in the string.
+ */
 module.exports.maxChar = function(str) {}
 
 
+/**
+ * CHALLENGE 12: FIZZ BUZZ
+ * @name fizzBuzz
+ * @description Fizz Buzz game, generate number from 0 to `max` and if the number is multiple of `n1` print "Fizz", if the number is multiple of `n2` print "Buzz", if is multiple of both print "FizzBuzz" else print the number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ *  
+ * @example
+ * fizzBuzz() //=> [0, 1, 2, Fizz, 4, Buzz ...etc]
+ * 
+ * 
+ * @param {Number} n1 Fizz number
+ * @param {Number} n2 Buzz number
+ * @param {Number} max Length of the array
+ * 
+ * @returns {Array} Returns an array of numbers
+ */
+module.exports.fizzBuzz = function({n1 = 3, n2 = 5, max = 100} = {}) {}
 
-// CHALLENGE 12: FIZZ BUZZ
-// DESCRIPTION: Fizz Buzz game, generate number from 0 to 100 and if the number is multiple of 3 print "Fizz", if the number is multiple of 5 print "Buzz", if is multiple of both print "FizzBuzz" else print the number.
-// Ex: fizzBuzz() => 0, 1, 2, Fizz, 4, Buzz ...etc
-module.exports.fizzBuzz = function() {}
+
+/**
+ * CHALLENGE 13: SIMPLE ADDING
+ * @name simpleAdding
+ * @description Write a function that sums all numbers from 0 to the given number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * simpleAdding(3) //=> 6
+ * 
+ * 
+ * @param {Number} num Counter 
+ * 
+ * @returns {Number} Returns sum of all numbers from 0 to `counter`
+ */
+module.exports.simpleAdding = function(num) {}
 
 
-// CHALLENGE 13: SIMPLE ADDING
-// DESCRIPTION: Write a function that sums all numbers from 0 to the given number.
-// Ex: simpleAdding(3) => 6 (1 + 2 + 3 )
-module.exports.SimpleAdding = function(num) {}
+/**
+ * CHALLENGE 14: ARRAY TO TREE
+ * @name arrayToTree
+ * @description Transform an array into a tree like object.
+ * @author Nenad Vracar (http://nenadvracar.com)
+ * 
+ * @see https://stackoverflow.com/questions/48951551/what-is-the-most-efficient-way-to-transform-an-array-of-array-of-string-to-a-tre
+ * 
+ * @example Usage:
+ * arrayToTree(['folder', 'subfolder', 'file.txt']) //=> [{name: 'folder', children: [ { name: 'subfolder', children: [ {name: 'file.txt'} ]} ]}]
+ * 
+ * 
+ * @param {Array} paths Array of the items to convert 
+ * 
+ * @returns {Array} Returns the tree generated from `paths`
+ */
+module.exports.arrayToTree = function (...paths) {}
+
+
+/**
+ * CHALLENGE 14: ARRAY TO TREE
+ * @name alphabeticallySort
+ * @description Write a function that can be used into an Array.sort() for sorting items alphabetically.
+ * @author Nachiketha <nachikethashu@gmail.com>
+ * 
+ * @see https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+ * 
+ * @example Usage:
+ * ['Italy', 'Canada', 'Australia'].sort(alphabeticallySort) //=> ['Australia', 'Canada', 'Italy']
+ * 
+ * 
+ * @param {String} a First argument of the `.sort` function 
+ * @param {String} b Second argument of the `.sort` function 
+ * 
+ * @returns {Boolean}
+ */
+module.exports.alphabeticallySort = function(a, b) {}
--- src/solutions.js
+++ src/solutions.js
@@ -1,6 +1,19 @@
-// CHALLENGE 1: ROUND NUMBER
-// DESCRIPTION: Write a function that round a number to given decimal places.
-// Ex: round(Math.PI, 2) => 3.14 (By default it will return 0 decimal places, round(Math.PI, 2) => 3)
+/**
+ * CHALLENGE 1: ROUND NUMBER
+ * @name round
+ * @description Write a function that round a number (`n`) to given decimal places.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ *
+ * 
+ * @example Usage:
+ * round(Math.PI, 2) // => 3.14
+ * 
+ * 
+ * @param {Number} n The number to be rounded
+ * @param {Number} places Decimal places
+ * 
+ * @returns {Number} Rounded number
+ */
 module.exports.round = function(n, places = 0) {
 	places = Math.pow(10, places)
 
@@ -8,25 +21,63 @@ module.exports.round = function(n, places = 0) {
 }
 
 
-// CHALLENGE 2: MERGE MULTIPLE ARRAYS
-// DESCRIPTION: Write a function that merges multiple given arrays.
-// Ex: ArrayMerge([1, 2], ['a', 'b']) => [1, 2, 'a', 'b']
+/**
+ * CHALLENGE 2: MERGE MULTIPLE ARRAYS
+ * @name arrayMerge
+ * @description Write a function that merges multiple given arrays.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * ArrayMerge([1, 2], ['a', 'b']) //=>  [1, 2, 'a', 'b']
+ * 
+ * 
+ * @param {Array} arr Arrays to merge
+ * 
+ * @returns {Array} Returns the merge of all arrays
+ */
 module.exports.arrayMerge = function(...arr) {
 	return arr.reduce((a, b) => [...a, ...b]);
 }
 
 
-// CHALLENGE 3: SUM CONTENT OF AN ARRAY
-// DESCRIPTION: Write a function to sum the content of an array.
-// Ex: sumArray([1, 2]) => 3
+/**
+ * CHALLENGE 3: SUM CONTENT OF AN ARRAY
+ * 
+ * @name arraySum
+ * @description Write a function to sum the content of an array
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * arraySum([10, 10, 10]) //=> 30
+ * 
+ * 
+ * @param {Array} arr Array to sum
+ * 
+ * @return {Number} The sum of all items in the array
+ */
 module.exports.arraySum = function(arr) {
 	return arr.reduce((a,b) => a+b);
 }
 
 
-// CHALLENGE 4: OBJECT FOREACH
-// DESCRIPTION: Write a forEach function that works with Objects.
-// Ex: objectForEach({ a: 1, b: 2, c: 3 }, console.log(key, value)) => a, 1; b, 2; etc...
+/**
+ * CHALLENGE 4: OBJECT FOREACH
+ * @name objectForEach
+ * @description ForEach function that works with Objects.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * objectForEach({ a:1, b:2 }, (key, value) => console.log(key, value)) //=> { a:1, b:2 }
+ * 
+ * 
+ * @param {Object} obj Main Object
+ * @param {Function} callback Function callback
+ * 
+ * @returns {Object} The start object
+ */
 module.exports.objectForEach = function(obj, callback) {
 	Object.keys(obj).forEach(key => callback(key, obj[key]));
 
@@ -34,31 +85,82 @@ module.exports.objectForEach = function(obj, callback) {
 }
 
 
-// CHALLENGE 5: STRING REVERSE
-// DESCRIPTION: Write a function that reverse a string.
-// Ex: reverseString('hello') => 'ollah'
+/**
+ * CHALLENGE 5: REVERSE STRING
+ * @name reverseString
+ * @description A function that reverse a string.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * reverseString('hello') //=> 'ollah'
+ * 
+ * 
+ * @param {String} str String to be reversed.
+ * 
+ * @returns {String} String reversed.
+ */
 module.exports.reverseString = function(str) {
 	return str.split('').reverse().join('')
 }
 
-// CHALLENGE 6: CHECK PALINDROME
-// DESCRIPITION: Write a function that checks if a word is a palindrome.
-// Ex: isPalindrome('racecar') => true
+
+/**
+ * CHALLENGE 6: CHECK PALINDROME
+ * @name isPalindrome
+ * @description A function that checks if a word is a palindrome.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * isPalindrome('racecar') //=> true
+ * 
+ * 
+ * @param {String} str String to be checked for palindrome.
+ * 
+ * @returns {Boolean} Return true if is a palindrome.
+ */
 module.exports.isPalindrome = function(str) {
 	return str.split('').reverse().join('') === str;
 }
 
-// CHALLENGE 7: IS MULTIPLE OF
-// DESCRIPTION: Write a function that checks if a number is multiple of another number.
-// Ex: isMultipleOf(3, 15) => true
+
+/**
+ * CHALLENGE 7: IS MULTIPLE OF
+ * @name isMultipleOf
+ * @description A function that checks if a number is multiple of another number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * isMultipleOf(3, 15) //=> true
+ * 
+ * 
+ * @param {Number} a Number to be checked for multiple of `b`
+ * @param {Number} b 
+ * 
+ * @returns {Boolean} If `a` is multiple of `b` returns true
+ */
 module.exports.isMultipleOf = function(a, b) {
 	return a % b === 0;
 }
 
 
-// CHALLENGE 8: GET THE LONGEST WORD
-// DESCRIPTION: Write a function that returns the longest word of a sentence.
-// Ex: longestWord('short loooong l0000ng') => 'loooong'
+/**
+ * CHALLENGE 8: GET THE LONGEST WORD
+ * @name longestWord
+ * @description A function that returns the longest word of a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * longestWord('short loooong l0000ng') //=> 'loooong'
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {String} Returns the longest word of `str`
+ */
 module.exports.longestWord = function(str) {
 	var words = str.trim().replace(/\W/g, ' ').trim().split(/\s+/);
 
@@ -70,9 +172,21 @@ module.exports.longestWord = function(str) {
 }
 
 
-// CHALLENGE 9: CAPITALIZE
-// DESCRIPTION: Write a function that capitalize each word in a sentence.
-// Ex: capitalize('hello world') => 'Hello World'
+/**
+ * CHALLENGE 9: CAPITALIZE
+ * @name capitalize
+ * @description A function that capitalize each word in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * capitalize('hello world') //=> 'Hello World'
+ * 
+ * 
+ * @param {String} str The string to be capitalized 
+ * 
+ * @returns {String} Returns a capitalized string
+ */
 module.exports.capitalize = function(str) {
 	var words = str.trim().split(/\s+/g)
 	return words.map(word => {
@@ -81,9 +195,22 @@ module.exports.capitalize = function(str) {
 }
 
 
-// CHALLENGE 10: VOWEL COUNT
-// DESCRIPTION: Write a function that count vowel in a sentence.
-// Ex: vowelCount('fox') => 1
+
+/**
+ * CHALLENGE 10: VOWEL COUNT
+ * @name vowelCount
+ * @description A function that count vowel in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * vowelCount('fox') //=> 1
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {Number} Returns number of vowels in `str`
+ */
 module.exports.vowelCount = function(str) {
 	let isVowel = /[aAeEiIoOuU]/g
 	return str.split('').filter(letter => {
@@ -92,9 +219,22 @@ module.exports.vowelCount = function(str) {
 }
 
 
-// CHALLENGE 11: MAX CHAR
-// DESCRIPTION: Get the most used char in a sentence.
-// Ex: maxChar('hello') => l
+
+/**
+ * CHALLENGE 11: MAX CHAR
+ * @name maxChar
+ * @description Get the most used char in a sentence.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * maxChar('hello') //=> l
+ * 
+ * 
+ * @param {String} str The string to be checked 
+ * 
+ * @returns {Number} Returns character most used in the string.
+ */
 module.exports.maxChar = function(str) {
 	let charMap = {};
 	let charCount = 0;
@@ -124,10 +264,25 @@ module.exports.maxChar = function(str) {
 }
 
 
-// CHALLENGE 12: FIZZ BUZZ
-// DESCRIPTION: Fizz Buzz game, generate number from 0 to 100 and if the number is multiple of 3 print "Fizz", if the number is multiple of 5 print "Buzz", if is multiple of both print "FizzBuzz" else print the number.
-// Ex: fizzBuzz() => 0, 1, 2, Fizz, 4, Buzz ...etc
+/**
+ * CHALLENGE 12: FIZZ BUZZ
+ * @name fizzBuzz
+ * @description Fizz Buzz game, generate number from 0 to `max` and if the number is multiple of `n1` print "Fizz", if the number is multiple of `n2` print "Buzz", if is multiple of both print "FizzBuzz" else print the number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ *  
+ * @example
+ * fizzBuzz() //=> [0, 1, 2, Fizz, 4, Buzz ...etc]
+ * 
+ * 
+ * @param {Number} n1 Fizz number
+ * @param {Number} n2 Buzz number
+ * @param {Number} max Length of the array
+ * 
+ * @returns {Array} Returns an array of numbers
+ */
 module.exports.fizzBuzz = function({n1 = 3, n2 = 5, max = 100} = {}) {
+	var numbers = [];
 	for(let i=0; i<=max; i++) {
 		let char = '';
 
@@ -139,15 +294,29 @@ module.exports.fizzBuzz = function({n1 = 3, n2 = 5, max = 100} = {}) {
 			char += 'Bar';
 		}
 
-		console.log( char || i );
+		numbers.push( char || i );
 	}
+
+	return numbers
 }
 
 
-// CHALLENGE 13: SIMPLE ADDING
-// DESCRIPTION: Write a function that sums all numbers from 0 to the given number.
-// Ex: simpleAdding(3) => 6 (1 + 2 + 3 )
-module.exports.SimpleAdding = function(num) {
+/**
+ * CHALLENGE 13: SIMPLE ADDING
+ * @name simpleAdding
+ * @description Write a function that sums all numbers from 0 to the given number.
+ * @author Federico Vitale <fedevitale99[at]gmail.com>
+ * 
+ * 
+ * @example Usage:
+ * simpleAdding(3) //=> 6
+ * 
+ * 
+ * @param {Number} num Counter 
+ * 
+ * @returns {Number} Returns sum of all numbers from 0 to `counter`
+ */
+module.exports.simpleAdding = function(num) {
   var numbers = [];
 
 	num = Math.abs(num)
@@ -159,4 +328,66 @@ module.exports.SimpleAdding = function(num) {
   }
 
   return numbers.reduce((a, b) => a+b);
+}
+
+
+/**
+ * CHALLENGE 14: ARRAY TO TREE
+ * @name arrayToTree
+ * @description Transform an array into a tree like object.
+ * @author Nenad Vracar (http://nenadvracar.com)
+ * 
+ * @see https://stackoverflow.com/questions/48951551/what-is-the-most-efficient-way-to-transform-an-array-of-array-of-string-to-a-tre
+ * 
+ * @example Usage:
+ * arrayToTree(['folder', 'subfolder', 'file.txt']) //=> [{name: 'folder', children: [ { name: 'subfolder', children: [ {name: 'file.txt'} ]} ]}]
+ * 
+ * 
+ * @param {Array} paths Array of the items to convert 
+ * 
+ * @returns {Array} Returns the tree generated from `paths`
+ */
+module.exports.arrayToTree = function (...paths) {
+    var result = [], tmp = { result }
+
+    paths.forEach(function (path) {
+        path.reduce(function (r, name, i) {
+            if (!r[name]) {
+                var o = { name }, children = []
+
+                r[name] = { result: children }
+
+                if (path[i + 1]) {
+                    o.children = children
+                }
+
+                r.result.push(o)
+            }
+            return r[name]
+        }, tmp)
+    })
+
+    return result;
+}
+
+
+/**
+ * CHALLENGE 14: ARRAY TO TREE
+ * @name alphabeticallySort
+ * @description Write a function that can be used into an Array.sort() for sorting items alphabetically.
+ * @author Nachiketha <nachikethashu@gmail.com>
+ * 
+ * @see https://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+ * 
+ * @example Usage:
+ * ['Italy', 'Canada', 'Australia'].sort(alphabeticallySort) //=> ['Australia', 'Canada', 'Italy']
+ * 
+ * 
+ * @param {String} a First argument of the `.sort` function 
+ * @param {String} b Second argument of the `.sort` function 
+ * 
+ * @returns {Boolean}
+ */
+module.exports.alphabeticallySort = function(a, b) {
+    return a.localeCompare(b) 
 }
