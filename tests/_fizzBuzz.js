5c1e922b6da9bf780645ff6f72d4d613f0a5c85d
Claudio Busatto <c.busatto@wille-enginering.com>
2018-03-14 11:24:47 +0100

    Add a simple test for FizzBuzz

--- tests/_fizzBuzz.js
+++ tests/_fizzBuzz.js
@@ -3,5 +3,10 @@ const {fizzBuzz} = require('..');
 const solutions = require('../src/solutions')
 
 test('FIZZ BUZZ', t => {
-	t.pass()
-})
+    var arguments = {n1: 3, n2: 5, max: 15};
+    var expectedResult = solutions.fizzBuzz(arguments);
+
+    var output = fizzBuzz(arguments);
+
+    return t.deepEqual(output, expectedResult);
+});
